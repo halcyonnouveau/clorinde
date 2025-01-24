4433dfa2c12a0cd6f93885c76c85423db99fffaf
alfowsec <120434096+alfowsec@users.noreply.github.com>
2025-01-24 17:54:18 +0530

    adding option to use depdency from workspace

--- crates/clorinde/src/codegen/cargo.rs
+++ crates/clorinde/src/codegen/cargo.rs
@@ -1,9 +1,8 @@
-use std::fmt::Write;
-
-use indoc::{formatdoc, writedoc};
 use postgres_types::{Kind, Type};
+use regex::Regex;
 
-use crate::config::{Config, CrateDependency};
+// use crate::config::{Config, CrateDependency, Package};
+use crate::config::Config;
 
 /// Register use of typed requiring specific dependencies
 #[derive(Debug, Clone, Default)]
@@ -43,253 +42,205 @@ impl DependencyAnalysis {
 }
 
 pub fn gen_cargo_file(dependency_analysis: &DependencyAnalysis, config: &Config) -> String {
+    let mut deps_toml = String::new();
     let package = config
         .package
         .to_string()
         .expect("unable to serialize package");
-
-    let mut buf = formatdoc! {r#"
-        # This file was generated with `clorinde`. Do not modify
-        {package}
-    "#};
-
-    if config.r#async {
-        let mut default_features = vec!["\"deadpool\""];
-        if dependency_analysis.has_dependency() && dependency_analysis.chrono {
-            default_features.push("\"chrono\"");
-        }
-
-        let mut wasm_features = vec!["\"tokio-postgres/js\""];
-        if dependency_analysis.has_dependency() && dependency_analysis.chrono {
-            wasm_features.push("\"chrono?/wasmbind\"");
-            wasm_features.push("\"time?/wasm-bindgen\"");
-        }
-
-        let default_features = default_features.join(", ");
-        let wasm_features = wasm_features.join(", ");
-
-        writedoc! { buf, r#"
-
-            [features]
-            default = [{default_features}]
-            deadpool = ["dep:deadpool-postgres", "tokio-postgres/default"]
-            wasm-async = [{wasm_features}]
-        "#}
-        .unwrap()
-    } else {
-        let mut wasm_features = vec![];
-        if dependency_analysis.has_dependency() && dependency_analysis.chrono {
-            wasm_features.push("\"chrono?/wasmbind\"");
-        }
-
-        let wasm_features = wasm_features.join(", ");
-
-        writedoc! { buf, r#"
-
-            [features]
-            default = []
-            wasm-sync = [{wasm_features}]
-        "#}
-        .unwrap();
-    }
-
-    if dependency_analysis.chrono {
-        writedoc! { buf, r#"
-
-            chrono = ["dep:chrono"]
-            time = ["dep:time"]
-        "#}
-        .unwrap();
-    } else {
-        writedoc! { buf, r#"
-
-            chrono = []
-            time = []
-        "#}
-        .unwrap();
-    }
-
-    writedoc! { buf, r#"
-
-        [dependencies]
-        ## Core dependencies
-        # Postgres types
-        postgres-types = {{ version = "0.2.8", features = ["derive"] }}
-        # Postgres interaction
-        postgres-protocol = "0.6.7"
-        # Iterator utils required for working with `postgres_protocol::types::ArrayValues`
-        fallible-iterator = "0.2.0"
-    "#}
-    .unwrap();
-
-    // add custom type crates
-    if !config.types.mapping.is_empty() {
-        let references_default_crate = config.types.mapping.values().any(|t| {
-            match t {
-                crate::config::TypeMapping::Simple(t) => t,
-                crate::config::TypeMapping::Detailed { rust_type, .. } => rust_type,
+    deps_toml.push_str("This file was generated with `clorinde`.Do not modify\n");
+    deps_toml.push_str(&package);
+    match config.r#async {
+        true => {
+            match (
+                dependency_analysis.has_dependency(),
+                dependency_analysis.chrono,
+            ) {
+                (true, true) => {
+                    deps_toml.push_str(
+                        "\n\
+                    [features]\n\
+                    default = [\"deadpool\",\"chrono\"]\n\
+                    deadpool = [\"dep:deadpool-postgres\",\"tokio-postgres\"]\n\
+                    wasm_async = [\"tokio-postgres/js\",\"chrono?/wasmbind\", \"time?/wasm-bindgen\"]    
+                    ",
+                    );
+                }
+                _ => deps_toml.push_str(
+                    "\n\
+                    [features]\n\
+                    default = [\"deadpool\"]\n\
+                    deadpool = [\"dep:deadpool-postgres\",\"tokio-postgres\"]\n\
+                    wasm_async = [\"tokio-postgres/js\"]
+                    ",
+                ),
             }
-            .starts_with("ctypes::")
-        });
-        if !config.types.crate_info.is_empty() {
-            for (name, dep) in &config.types.crate_info {
-                match dep {
-                    CrateDependency::Simple(version) => {
-                        writedoc! { buf, r#"
-                        {name} = "{version}"
-                    "#}
-                        .unwrap();
-                    }
-                    CrateDependency::Detailed {
-                        version,
-                        path,
-                        features,
-                        default_features,
-                        optional,
-                    } => {
-                        writedoc! { buf, r#"
-                        {name} = {{"#}
-                        .unwrap();
-
-                        let mut first = true;
-
-                        if let Some(version) = version {
-                            write!(buf, r#"version = "{version}""#).unwrap();
-                            first = false;
-                        }
-                        if let Some(path) = path {
-                            if !first {
-                                write!(buf, ", ").unwrap();
-                            }
-                            write!(buf, r#"path = "{path}""#).unwrap();
-                            first = false;
-                        }
-                        if let Some(features) = features {
-                            if !first {
-                                write!(buf, ", ").unwrap();
-                            }
-                            write!(buf, r#"features = ["#).unwrap();
-                            for (i, feature) in features.iter().enumerate() {
-                                if i > 0 {
-                                    write!(buf, ", ").unwrap();
-                                }
-                                write!(buf, r#""{feature}""#).unwrap();
-                            }
-                            write!(buf, "]").unwrap();
-                            first = false;
-                        }
-                        if let Some(default_features) = default_features {
-                            if !first {
-                                write!(buf, ", ").unwrap();
-                            }
-                            write!(buf, r#"default-features = {default_features}"#).unwrap();
-                            first = false;
-                        }
-                        if let Some(optional) = optional {
-                            if !first {
-                                write!(buf, ", ").unwrap();
-                            }
-                            write!(buf, r#"optional = {optional}"#).unwrap();
-                        }
-
-                        writedoc! { buf, r#"}}
-                        "#}
-                        .unwrap();
-                    }
+        }
+        _ => {
+            match (
+                dependency_analysis.has_dependency(),
+                dependency_analysis.chrono,
+            ) {
+                (true, true) => {
+                    deps_toml.push_str(
+                        "\n\
+                    [features]\n\
+                    default = []\n\
+                    wasm_sync = [\"chrono?/wasmbind\"]    
+                    ",
+                    );
+                }
+                _ => {
+                    deps_toml.push_str(
+                        "\n\
+                    [features]\n\
+                    default = []\n\
+                    wasm_sync = []    
+                    ",
+                    );
                 }
             }
-        } else if references_default_crate {
-            writedoc! { buf, r#"
-            ctypes = {{ path = "../ctypes" }}
-        "#}
-            .unwrap();
+        }
+    };
+    match dependency_analysis.chrono {
+        true => {
+            deps_toml.push_str(
+                "\n\
+            chrono = [\"dep:chrono\"]\n\
+            time = [\"dep:time\"]
+            ",
+            );
+        }
+        _ => {
+            deps_toml.push_str(
+                "\n\
+            chrono = []\n\
+            time = []
+            ",
+            );
+        }
+    };
+    deps_toml.push_str(
+        "\n\
+        [dependencies]\n\
+        ## Core dependencies\n\
+        # Postgres types\n\
+        postgres-types = {\"0.2.8\", features = [\"derive\"] }\n\
+        # Postgres interaction\n\
+        postgres-protocol = {\"0.6.7\"}\n\
+        # Iterator utils required for working with `postgres_protocol::types::ArrayValues`\n\
+        fallible-iterator = {\"0.2.0\"}
+        ",
+    );
+    match dependency_analysis.has_dependency() {
+        true => deps_toml.push_str(
+            "\n\
+            ## Types dependencies\n\
+            # TIME, DATE, TIMESTAMP or TIMESTAMPZ\n\
+            chrono = {\"0.4.39\", optional = true }
+            time = {\"0.3.37\", optional = true }
+        ",
+        ),
+        _ => {}
+    }
+    match dependency_analysis.uuid {
+        true => deps_toml.push_str(
+            "\n\
+            # UUID
+            uuid = {\"1.11.0\"}
+        ",
+        ),
+        _ => {}
+    };
+    match dependency_analysis.mac_addr {
+        true => deps_toml.push_str(
+            "\n\
+           # MAC ADDRESS\n\
+           eui48 = {\"1.1.0\", default-features = false }
+        ",
+        ),
+        _ => {}
+    };
+    match dependency_analysis.decimal {
+        true => deps_toml.push_str(
+            "\n\
+            # DECIMAL\n\
+            rust_decimal = {\"1.36.0\", features = [\"db-postgres\"] }
+        ",
+        ),
+        _ => {}
+    };
+    match dependency_analysis.uuid {
+        true => deps_toml.push_str(
+            "\n\
+            # JSON or JSONB\n\
+            serde_json = {\"1.0.134\", features = [\"raw_value\"] }\n\
+            serde = {\"1.0.217\", features = [\"derive\"] }\n\
+        ",
+        ),
+        _ => {}
+    };
+    match (config.serialize, !dependency_analysis.json) {
+        (true, true) => deps_toml.push_str(
+            "\n\
+            ## Serialize\n\
+            serde = {\"1.0.217\", features = [\"derive\"] }
+        ",
+        ),
+        _ => {}
+    };
+    match config.sync {
+        true => deps_toml.push_str(
+            "\n\
+            # UUID\n\
+            uuid = {\"1.11.0\"} ## Sync client dependencies\n\
+            # Postgres sync client\n\
+            postgres = {\"0.19.9\", features = [{client_features}] }
+        ",
+        ),
+        _ => {}
+    };
+    match config.r#async {
+        true => deps_toml.push_str(
+            "\n\
+            ## Async client dependencies\n\
+            # Async utils\n\
+            futures = {\"0.3.31\"}\n\
+            \n\
+            ## Async features dependencies\n\
+            # Async connection pooling\n\
+            deadpool-postgres = {\"0.14.1\", optional = true }
+        ",
+        ),
+        _ => {}
+    };
+    match (config.r#async, dependency_analysis.chrono) {
+        (true, true) => deps_toml.push_str(
+            "\n\
+            # Postgres async client\n\
+            tokio-postgres = {\"0.7.12\", default-features = false, features = [\"with-chrono-0_4\",\"with-time-0_3\"]}\n\
+        ",
+        ),
+        _ => deps_toml.push_str(
+            "\n\
+            # Postgres async client\n\
+            tokio-postgres = {\"0.7.12\", default-features = false, features = []}\n\
+        ",
+        ),
+    };
+
+    match config.workspace {
+        true => {
+            let re = Regex::new(r#"\{"(\d+(\.\d+)*)""#).unwrap();
+            let deps_toml = re.replace_all(&deps_toml, "{ workspace = true ");
+            deps_toml.to_string()
+        }
+        _ => {
+            let re = Regex::new(r#"\{"(.*?)\""#).unwrap();
+            let deps_toml = re.replace_all(&deps_toml, |caps: &regex::Captures| {
+                let matched_number = &caps[1];
+                format!("{{version = \"{}\"", matched_number)
+            });
+            deps_toml.to_string()
         }
     }
-
-    let mut client_features = String::new();
-
-    if dependency_analysis.has_dependency() {
-        writeln!(buf, "\n## Types dependencies").unwrap();
-        if dependency_analysis.chrono {
-            writedoc! { buf, r#"
-                # TIME, DATE, TIMESTAMP or TIMESTAMPZ
-                chrono = {{ version = "0.4.39", optional = true }}
-                time = {{ version = "0.3.37", optional = true }}
-            "#}
-            .unwrap();
-            write!(client_features, r#""with-chrono-0_4","#).unwrap();
-            write!(client_features, r#""with-time-0_3","#).unwrap();
-        }
-        if dependency_analysis.uuid {
-            writedoc! { buf, r#"
-                # UUID
-                uuid = "1.11.0"
-            "#}
-            .unwrap();
-            write!(client_features, r#""with-uuid-1","#).unwrap();
-        }
-        if dependency_analysis.mac_addr {
-            writedoc! { buf, r#"
-                # MAC ADDRESS
-                eui48 = {{ version = "1.1.0", default-features = false }}
-            "#}
-            .unwrap();
-            write!(client_features, r#""with-eui48-1","#).unwrap();
-        }
-        if dependency_analysis.decimal {
-            writedoc! { buf, r#"
-                # DECIMAL
-                rust_decimal = {{ version = "1.36.0", features = ["db-postgres"] }}
-            "#}
-            .unwrap();
-        }
-
-        if dependency_analysis.json {
-            writedoc! { buf, r#"
-                # JSON or JSONB
-                serde_json = {{ version = "1.0.134", features = ["raw_value"] }}
-                serde = {{ version = "1.0.217", features = ["derive"] }}
-            "#}
-            .unwrap();
-            write!(client_features, r#""with-serde_json-1","#).unwrap();
-        }
-    }
-
-    // add serde if serializing but not using json type
-    if config.serialize && !dependency_analysis.json {
-        writedoc! { buf, r#"
-
-            ## Serialize
-            serde = {{ version = "1.0.217", features = ["derive"] }}
-        "#}
-        .unwrap();
-    }
-
-    if config.sync {
-        writedoc! { buf, r#"
-
-            ## Sync client dependencies
-            # Postgres sync client
-            postgres = {{ version = "0.19.9", features = [{client_features}] }}
-        "#}
-        .unwrap();
-    }
-
-    if config.r#async {
-        writedoc! { buf, r#"
-
-            ## Async client dependencies
-            # Postgres async client
-            tokio-postgres = {{ version = "0.7.12", default-features = false, features = [{client_features}] }}
-            # Async utils
-            futures = "0.3.31"
-
-            ## Async features dependencies
-            # Async connection pooling
-            deadpool-postgres = {{ version = "0.14.1", optional = true }}
-        "#}
-        .unwrap();
-    }
-
-    buf
 }
