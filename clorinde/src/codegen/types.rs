8b3db09f0f5d4dd0d33a72258839f926beb670a8
Jack Lee <jack.y.l.dev@gmail.com>
2025-02-03 15:21:16 -0500

    update

--- clorinde/src/codegen/types.rs
+++ clorinde/src/codegen/types.rs
@@ -14,14 +14,14 @@ pub(crate) fn gen_type_modules(
     config: &Config,
 ) -> proc_macro2::TokenStream {
     let mut tokens = quote! {
-        #[cfg(all(feature = "chrono", not(feature = "time")))]
+        #[cfg(feature = "chrono")]
         pub mod time {
             pub type Timestamp = chrono::NaiveDateTime;
             pub type TimestampTz = chrono::DateTime<chrono::FixedOffset>;
             pub type Date = chrono::NaiveDate;
             pub type Time = chrono::NaiveTime;
         }
-        #[cfg(all(feature = "time", not(feature = "chrono")))]
+        #[cfg(feature = "time")]
         pub mod time {
             pub type Timestamp = time::PrimitiveDateTime;
             pub type TimestampTz = time::OffsetDateTime;
