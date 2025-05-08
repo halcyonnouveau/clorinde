63584325b5cd99a2ba0b5dce63f409f2fd37e17d
Folyd <lyshuhow@gmail.com>
2025-05-08 04:38:31 +0000

    fix: update build.rs to use correct clorinde API

--- examples/enum_fromstr/enum_fromstr_codegen/build.rs
+++ examples/enum_fromstr/enum_fromstr_codegen/build.rs
@@ -1,27 +1,24 @@
-use std::error::Error;
+use clorinde::{Error, config::Config};
 use std::path::Path;
 
-fn main() -> Result<(), Box<dyn Error>> {
+#[allow(clippy::result_large_err)]
+fn main() -> Result<(), Error> {
     println!("cargo:rerun-if-changed=../queries");
     println!("cargo:rerun-if-changed=../schema.sql");
 
-    let out_dir = std::env::var("OUT_DIR").unwrap();
     let manifest_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
     let parent_dir = Path::new(&manifest_dir).parent().unwrap();
 
     let queries_path = parent_dir.join("queries");
     let schema_path = parent_dir.join("schema.sql");
 
-    clorinde::generate_to_file(
-        clorinde::Config::new(
-            out_dir,
-            schema_path,
-            queries_path,
-            "enum_fromstr_codegen".to_string(),
-            true,
-        )
-        .verbose(true),
-    )?;
+    let cfg = Config::builder()
+        .name("enum_fromstr_codegen")
+        .destination(std::env::var("OUT_DIR").unwrap())
+        .queries(queries_path)
+        .build();
+
+    clorinde::gen_managed(&[schema_path], cfg)?;
 
     Ok(())
 }
